af2813ccf6a6d570bb0f93cbd9e7d2ed0961c82e
JavaCommons Technologies <javacommons@gmail.com>
2026-02-07 02:19:20 +0900

    .

---
+++ JsonApiClient.cs
@@ -0,0 +1,93 @@
+namespace Global
+{
+    using System;
+    using System.Reflection;
+    using System.Runtime.InteropServices;
+
+    internal class JsonApiClient
+    {
+        IntPtr Handle = IntPtr.Zero;
+        IntPtr CallPtr = IntPtr.Zero;
+        delegate IntPtr proto_Call(IntPtr name, IntPtr args);
+        public JsonApiClient(string dllSpec)
+        {
+            string dllPath = Sys.FindExePath(dllSpec);
+            if (dllPath is null)
+            {
+                EasyObject.Log(dllSpec, "dllSpec");
+                EasyObject.Log(dllPath, "dllPath");
+                Environment.Exit(1);
+            }
+            this.LoadDll(dllPath);
+        }
+        public JsonApiClient(string dllSpec, string cwd)
+        {
+            string dllPath = Sys.FindExePath(dllSpec, cwd);
+            if (dllPath is null)
+            {
+                EasyObject.Log(dllSpec, "dllSpec");
+                EasyObject.Log(dllPath, "dllPath");
+                Environment.Exit(1);
+            }
+            this.LoadDll(dllPath);
+        }
+        public JsonApiClient(string dllSpec, Assembly assembly)
+        {
+            string dllPath = Sys.FindExePath(dllSpec, assembly);
+            if (dllPath is null)
+            {
+                EasyObject.Log(dllSpec, "dllSpec");
+                EasyObject.Log(dllPath, "dllPath");
+                Environment.Exit(1);
+            }
+            this.LoadDll(dllPath);
+        }
+        private void LoadDll(string dllPath)
+        {
+            this.Handle = Sys.LoadLibraryExW(
+                dllPath,
+                IntPtr.Zero,
+                Sys.LoadLibraryFlags.LOAD_WITH_ALTERED_SEARCH_PATH
+                );
+            if (this.Handle == IntPtr.Zero)
+            {
+                EasyObject.Log($"DLL not loaded: {dllPath}");
+                Environment.Exit(1);
+            }
+            this.CallPtr = Sys.GetProcAddress(Handle, "Call");
+            if (this.CallPtr == IntPtr.Zero)
+            {
+                EasyObject.Log("Call() not found");
+                Environment.Exit(1);
+            }
+        }
+        public EasyObject Call(string name, EasyObject args)
+        {
+            IntPtr pName = Sys.StringToUTF8Addr(name);
+            proto_Call pCall = (proto_Call)Marshal.GetDelegateForFunctionPointer(this.CallPtr, typeof(proto_Call));
+            var argsJson = args.ToJson();
+            IntPtr pArgsJson = Sys.StringToUTF8Addr(argsJson);
+            IntPtr pResult = pCall(pName, pArgsJson);
+            string result = Sys.UTF8AddrToString(pResult);
+            result = result.Trim();
+            Marshal.FreeHGlobal(pName);
+            Marshal.FreeHGlobal(pArgsJson);
+            if (result.StartsWith("\""))
+            {
+                string error = EasyObject.FromJson(result).Cast<string>();
+                throw new Exception(error);
+            }
+            else if (result.StartsWith("["))
+            {
+                var list = EasyObject.FromJson(result);
+                if (list.Count == 0) return EasyObject.FromObject(null);
+                return list[0];
+            }
+            else
+            {
+                string error = $"Malformed result json: {result}";
+                throw new Exception(error);
+            }
+        }
+    }
+}
---
+++ JsonApiServer.cs
@@ -0,0 +1,80 @@
+namespace Global
+{
+    using System;
+    using System.Reflection;
+    using System.Threading;
+    using static Global.EasyObject;
+
+    internal class JsonApiServer
+    {
+        IntPtr Handle = IntPtr.Zero;
+        IntPtr CallPtr = IntPtr.Zero;
+        IntPtr LastErrorPtr = IntPtr.Zero;
+        delegate IntPtr proto_Call(IntPtr name, IntPtr args);
+        public JsonApiServer()
+        {
+        }
+        static ThreadLocal<IntPtr> HandleCallPtr = new ThreadLocal<IntPtr>();
+        public IntPtr HandleNativeCall(Type apiType, IntPtr nameAddr, IntPtr inputAddr)
+        {
+            if (HandleCallPtr.Value != IntPtr.Zero)
+            {
+                Sys.FreeHGlobal(HandleCallPtr.Value);
+                HandleCallPtr.Value = IntPtr.Zero;
+            }
+            var name = Sys.UTF8AddrToString(nameAddr);
+            var input = Sys.UTF8AddrToString(inputAddr);
+            EasyObject args = FromJson(input);
+            MethodInfo mi = apiType.GetMethod(name);
+            dynamic result = null;
+            if (mi == null)
+            {
+                result = $"API not found: {name}";
+            }
+            else
+            {
+                try
+                {
+                    result = mi.Invoke(null, new object[] { args });
+                    EasyObject okResult = EasyObject.EmptyArray;
+                    okResult.Add(result);
+                    result = okResult;
+                }
+                catch (TargetInvocationException ex)
+                {
+                    result = ex.InnerException.ToString();
+                }
+            }
+            string output = FromObject(result).ToJson();
+            HandleCallPtr.Value = Sys.StringToUTF8Addr(output);
+            return HandleCallPtr.Value;
+        }
+        public string HandleDotNetCall(Type apiType, string name, string input)
+        {
+            EasyObject args = FromJson(input);
+            MethodInfo mi = apiType.GetMethod(name);
+            dynamic result = null;
+            if (mi == null)
+            {
+                result = $"API not found: {name}";
+            }
+            else
+            {
+                try
+                {
+                    result = mi.Invoke(null, new object[] { args });
+                    EasyObject okResult = EasyObject.EmptyArray;
+                    okResult.Add(result);
+                    result = okResult;
+                }
+                catch (TargetInvocationException ex)
+                {
+                    result = ex.InnerException.ToString();
+                }
+            }
+            string output = FromObject(result).ToJson();
+            return output;
+        }
+    }
+
+}
