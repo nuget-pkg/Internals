8461767c80748bba4b0adbf4c49ad0a5f16efe7f
JavaCommons Technologies <javacommons@gmail.com>
2026-02-08 08:07:24 +0900

    .

--- JsonApiServer.cs
+++ JsonApiServer.cs
@@ -2,6 +2,7 @@ namespace Global
 {
     using System;
     using System.Reflection;
+    using System.Text;
     using System.Threading;
     using static Global.EasyObject;
 
@@ -46,6 +47,41 @@ namespace Global
                 }
             }
             string output = FromObject(result).ToJson();
+            HandleCallPtr.Value = Sys.StringToUTF8Addr(output);
+            return HandleCallPtr.Value;
+        }
+        public IntPtr HandleNativeCallBase64(Type apiType, IntPtr nameAddr, IntPtr inputAddr)
+        {
+            if (HandleCallPtr.Value != IntPtr.Zero)
+            {
+                Sys.FreeHGlobal(HandleCallPtr.Value);
+                HandleCallPtr.Value = IntPtr.Zero;
+            }
+            var name = Sys.UTF8AddrToString(nameAddr);
+            var input = Sys.UTF8AddrToString(inputAddr);
+            EasyObject args = FromJson(input)!;
+            MethodInfo? mi = apiType.GetMethod(name);
+            dynamic? result = null;
+            if (mi == null)
+            {
+                result = $"API not found: {name}";
+            }
+            else
+            {
+                try
+                {
+                    result = mi.Invoke(null, new object[] { args });
+                    EasyObject okResult = EasyObject.EmptyArray;
+                    okResult.Add(result);
+                    result = okResult;
+                }
+                catch (TargetInvocationException ex)
+                {
+                    result = ex.InnerException!.ToString();
+                }
+            }
+            string output = FromObject(result).ToJson();
+            output = Convert.ToBase64String(Encoding.UTF8.GetBytes(output));
             HandleCallPtr.Value = Sys.StringToUTF8Addr(output);
             return HandleCallPtr.Value;
         }
